b4aab5120b484c10e1f6af195c69909427aea288
Kayc Prado <30593578+kaycps@users.noreply.github.com>
2018-06-04 21:13:40 -0300

    First commit

---
+++ resources/views/states/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Estados
+                    <a href="/state/create" class="float-right btn btn-success">Novo Estado</a>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Sigla</th>
+                            <th>Ações</th>
+                        </tr>
+                        
+                        @foreach($states as $p)
+                            <tr>
+                                <td>{{ $p->id }}</td>
+                                <td>{{ $p->nome_estado }}</td>
+                                <td>{{ $p->sigla }}</td>
+                                <td>
+                                    <a href="/state/{{ $p->id }}/edit" class="btn btn-warning">Editar</a>
+                                    
+                                    {!! Form::open(['url' => "/state/$p->id", 'method' => 'delete']) !!}
+                                        {{ Form::submit('Deletar', null, ['class' => 'btn btn-danger']) }}
+                                    {!! Form::close() !!}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
